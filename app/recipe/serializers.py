392bfb09b55ecb0e09b39d08b269e13eacc3baa1
dhal.pratapbhanu@gmail.com <dhal.pratapbhanu@gmail.com>
2020-03-29 01:26:15 +0530

    Added Create Recipe Feature

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -36,7 +36,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         model = Recipe
         fields = (
             'id', 'title', 'ingredients', 'tags', 'time_minutes',
-            'price', 'link'
+            'price', 'link',
         )
         read_only_fields = ('id',)
 
